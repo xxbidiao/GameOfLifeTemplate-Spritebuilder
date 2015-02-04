d2b8c805314935ed0d0633db6aebdba3e08a9119
LinLee <linlee@LinLees-Air.sv.cmu.local>
2015-02-03 18:09:15 -0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/MainScene.m
+++ GameOfLife.spritebuilder/Source/MainScene.m
@@ -41,9 +41,9 @@
 // this method will get called every half second when you hit the play button and will stop getting called when you hi the pause button
 - (void)step
 {
-    [_grid evolveStep];
-    _generationLabel.string = [NSString stringWithFormat:@"%d", _grid.generation];
-    _populationLabel.string = [NSString stringWithFormat:@"%d", _grid.totalAlive];
+    //[_grid evolveStep];
+    //_generationLabel.string = [NSString stringWithFormat:@"%d", _grid.generation];
+    //_populationLabel.string = [NSString stringWithFormat:@"%d", _grid.totalAlive];
 }
 
 @end
