00e699a6d697d98c85d1e9d8cb42ee2b41f5b566
LinLee <linlee@LinLees-Air.sv.cmu.local>
2015-02-03 17:39:39 -0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.h
+++ GameOfLife.spritebuilder/Source/Grid.h
@@ -13,4 +13,14 @@
 @property (nonatomic, assign) int totalAlive;
 @property (nonatomic, assign) int generation;
 
+-(void)evolveStep;
+
+-(void)countNeighbors;
+
+- (BOOL)isIndexValidForX:(int)x andY:(int)y;
+
+-(void)updateCreatures;
+
+
+
 @end
