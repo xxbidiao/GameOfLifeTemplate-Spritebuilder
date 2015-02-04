467acc557667bfaffcdf765111a7bbd6465d01b4
LinLee <linlee@LinLees-Air.sv.cmu.local>
2015-02-03 18:09:46 -0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/MainScene.m
+++ GameOfLife.spritebuilder/Source/MainScene.m
@@ -42,8 +42,8 @@
 - (void)step
 {
     //[_grid evolveStep];
-    //_generationLabel.string = [NSString stringWithFormat:@"%d", _grid.generation];
-    //_populationLabel.string = [NSString stringWithFormat:@"%d", _grid.totalAlive];
+    _generationLabel.string = [NSString stringWithFormat:@"%d", _grid.generation];
+    _populationLabel.string = [NSString stringWithFormat:@"%d", _grid.totalAlive];
 }
 
 @end
